89f49456ceb0d08e2edd53ab1eca150730244c38
Nagul Meera Shaik <161418040+Nagul-Tekworks@users.noreply.github.com>
2024-12-09 16:37:25 +0530

    Add files via upload

---
+++ LitzChill-Backend/supabase/functions/_routes/RoutesPaths.ts
@@ -0,0 +1,18 @@
+// export const CONTEST_ROUTES = {
+//     CONTEST_CREATE_PATH: "/ContestModule/createContest",
+//     CONTEST_GET_ALL_PATH: "/ContestModule/getAllContest",
+//     CONTEST_GET_BY_ID_PATH: "/ContestModule/getContestById/:id",
+//     CONTEST_UPDATE_BY_ID_PATH: "/ContestModule/updateContestById/:id",
+//     CONTEST_DELETE_BY_ID_PATH: "/ContestModule/deleteContestById/:id",
+// }
+
+// export const COMMENT_ROUTES = {
+//     COMMENT_ADD_PATH: "/CommentAPI/addComment",
+//     COMMENT_ADD_TO_OTHER:"CommentAPI/addCommentToOther/:id",
+//     COMMENT_DELETE_BY_ID_PATH: "/CommentAPI/deleteComment/:id"
+
+// }
+
+// export const FLAG_ROUTES = {
+//     ADD_FLAG_TO_MEME: "/FlagAPI/addFlag"
+// }
---
+++ LitzChill-Backend/supabase/functions/_shared/_messages/ErrorMessages.ts
@@ -0,0 +1,12 @@
+//commmon error messages for all modules.
+export const COMMON_ERROR_MESSAGES={
+    METHOD_NOT_ALLOWED: "This method is not allowed for the requested operation.",
+    ROUTE_NOT_FOUND: "The requested route was not found. Please verify the URL.",
+    INTERNAL_SERVER_ERROR: "Something went wrong. Please try again later.",
+    EMPTY_REQUEST_BODY: "The request body is empty. Please provide valid data.",
+    DATABASE_ERROR: "Database error occurred. Please try again.",
+    MISSING_JWT_TOKEN: "Missing JWT token in the request.",
+    INVALID_JWT_TOKEN: " The provided JWT token is invalid or expired.",
+    UNAUTHORIZED_ACCESS: "You do not have the necessary permissions to access this resource.",
+};
+
